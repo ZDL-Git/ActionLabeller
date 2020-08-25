37010c41c7113d6db88b75213a0ace341acffe32
ZDL <zdl_daily@163.com>
2020-08-25 17:32:23 +0800

    1.添加app图标

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
 from view.MainWindow import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('resources/app_icon2.png'))
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
